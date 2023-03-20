ba7b5022d085baf91b2e04d0fa5fbe30b6a855db
rwberreman <128352876+rwberreman@users.noreply.github.com>
2023-03-20 10:06:54 -0700

    Update and rename Challenge1.py to Challenge1&2.py

--- Challenge1&2.py
+++ Challenge1&2.py
@@ -1,12 +1,8 @@
-from zipfile import ZipFile
-from bs4 import BeautifulSoup
+# For Windows OS
 
-import requests
-import ctypes
-import os
-import shutil
-import sys
-import wget
+import requests, os, ctypes, sys, wget, shutil
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
 
 # Function for running commands with elevated permissions, for copying files into Program Files
 # https://stackoverflow.com/questions/130763/request-uac-elevation-from-within-a-python-script
@@ -15,15 +11,15 @@ def is_admin():
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
-
+    
 # Using the above function to check for, and create the folders relevant to this script
 if is_admin():
-    os.chdir(r'C:\\Program Files')
+    os.chdir(r'C:\\Program Files\\')
     if not os.path.exists("Terraform"):
-        os.mkdirs("Terraform")
+        os.mkdir("Terraform")
     os.chdir(r'C:\\Program Files\\Terraform')
     if not os.path.exists("Download"):
-        os.mkdirs("Download")
+        os.mkdir("Download")
     if not os.path.exists("Archive"):
         os.mkdir("Archive")
 else:
@@ -33,7 +29,7 @@ FilePath = r'C:\\Program Files\\Terraform\\Download'
 
 # Take the version number from the existing file in the Downloads folder
 # If no files are present, set the Current Version to 0.0.0
-if len(FilePath) == 0:
+if len(FilePath) != 0:
     FileNames = os.listdir(FilePath)
     FileName = ''.join(FileNames)
     FileParts = FileName.split('_')
@@ -46,6 +42,7 @@ MainURL = 'https://releases.hashicorp.com/terraform/'
 DownloadLink = 'https://releases.hashicorp.com'
 response = requests.get(MainURL)
 soup = BeautifulSoup(response.text, 'html.parser')
+
 Links = []
 for Link in soup.find_all('a'):
     Links.append(Link.get('href'))
@@ -61,6 +58,7 @@ if CurrentVersion not in Links[LinkLocation] and "-" not in Links[LinkLocation]:
                     + '_windows_386.zip')
     DownloadName = 'terraform_' + LatestVersion[2] + '_windows_386'
 
+    RootPath = r'C:\\Program Files\\Terraform'
     SavePath = r'C:\\Program Files\\Terraform\\Download'
     Version = LatestVersion[2]
 
@@ -68,18 +66,30 @@ if CurrentVersion not in Links[LinkLocation] and "-" not in Links[LinkLocation]:
     ExeFile = os.path.join(SavePath, 'terraform.exe')
 
     # Running with elevated permissions to first download the file to Program Files\Terraform\Downloads
-    # Then unzip the zip file, rename the .exe to contain the version number, and then move it down a directory
+    # Then unzip the zip file, and then move it down a directory.
+    # For challenge 3, if there is a new version, but already a Terraform.exe in Program Files\Terraform, rename it with the version number,
+    # and move it to the Archive folder.
     if is_admin():
         for file in os.listdir(SavePath):
             shutil.move(os.path.join(SavePath, file), r'C:\\Program Files\\Terraform\\Archive')
         wget.download(DownloadLink, out=SavePath)
         with ZipFile(DownloadFile, 'r') as ZipObject:
             ZipObject.extractall(FilePath)
-        os.rename(ExeFile, os.path.join(SavePath, 'terraform_' + Version + '.exe'))
-        os.chdir(SavePath)
 
-        shutil.move(os.path.join(SavePath, 'terraform_' + Version + '.exe'), r'C:\\Program Files\\Terraform')
+        os.chdir(SavePath)
+        if os.path.isfile(r'C:\\Program Files\\Terraform\\terraform.exe'):
+            os.rename(os.path.join(RootPath, 'terraform.exe'), os.path.join(RootPath, 'terraform_' + CurrentVersion + '.exe'))
+            shutil.move(os.path.join(RootPath, 'terraform_' + CurrentVersion + '.exe'), r'C:\\Program Files\\Terraform\\Archive')
+        shutil.move(os.path.join(SavePath, 'terraform.exe'), r'C:\\Program Files\\Terraform')
     else:
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 else:
     print('Already on latest version.')
+    
+# Things to Improve
+# Better method of checking version. Running terraform -version in CMD returns version, maybe see if that value can be pulled.
+# Better checking of versioning on site, logic is shaky currently and relies on the newest version being at the top of the page.
+#       Set up some loop to check previous versions. Like if the latest version (one at the top) is beta, go to next link, if version number is greater than installed
+#       version number, download that file.
+# Not entirely positive whether backing up the old .exe also backs up the Terraform binary, need to investigate further.
+# This works on my personal computer, but not work . Unsure if this is due to more strict restrictions on the work computer. Need to investigate.
